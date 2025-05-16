fd08e271274c0f6b19fd1ed77ecb84e5d7ccdfd8
Salgado <lucas.lemos.bertao@gmail.com>
2025-05-16 16:26:24 -0300

    Foi adicionado programas funcionais nos arquivos de programa do Arduino IDE

--- Controle_Servo_Motor/Controle_Servo_Motor.ino
+++ Controle_Servo_Motor/Controle_Servo_Motor.ino
@@ -1,9 +1,17 @@
 void setup() {
   // put your setup code here, to run once:
 
+  pinMode(A0,INPUT);
+  pinMode(9,OUTPUT);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  int S = analogRead(A0);
+  int S_mapped = map(S,0,1023,0,255);
+
+  analogWrite(9,S_mapped);
 
 }
--- Pisca_LED/Pisca_LED.ino
+++ Pisca_LED/Pisca_LED.ino
@@ -1,9 +1,17 @@
 void setup() {
   // put your setup code here, to run once:
 
+  pinMode(5,OUTPUT);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  analogWrite(5,153);
+  delay(500);
+  analogWrite(5,153);
+  delay(500);
+  
 
 }
