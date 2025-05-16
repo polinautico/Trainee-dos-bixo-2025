e42df5d96cf3f38b94b858a45d895c3145617785
LucasDipper <lucaslameusilvavieira@gmail.com>
2025-05-16 16:24:34 -0300

    Adicionei código Picas_Led e Contorle_Servo_Motor

--- Controle_Servo_Motor/Controle_Servo_Motor.ino
+++ Controle_Servo_Motor/Controle_Servo_Motor.ino
@@ -1,9 +1,13 @@
-void setup() {
-  // put your setup code here, to run once:
+#define potenciometro A0
+#define servo 9
 
+void setup() {
+  pinMode(potenciometro, INPUT);
+  pinMode(servo, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  int x = analogRead(potenciometro);
+  int mappedValue = map(x, 0, 1023, 0, 255);
+  analogWrite(servo, mappedValue);
 }
--- Pisca_LED/Pisca_LED.ino
+++ Pisca_LED/Pisca_LED.ino
@@ -1,9 +1,15 @@
+#define Led 3
+
 void setup() {
+  pinMode(Led, OUTPUT)
   // put your setup code here, to run once:
 
 }
 
 void loop() {
+  analogWrite(Led, 153);
+  delay(1000);
+  analogWrite(Led, 0);
+  delay(1000);
   // put your main code here, to run repeatedly:
-
 }
