2c8da0a3b99b391921ad15637e0e77d0611b94c2
unknown <lucaslameusilvavieira@gmail.com>
2026-01-27 01:31:21 -0300

    Adicionando codigo de teste do sensor de corrente

--- Dipper/Sensor corrente/src/main.cpp
+++ Dipper/Sensor corrente/src/main.cpp
@@ -1,18 +1,21 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
+#define sensor A0
+int leitura_amp = 0;
+float V_lida = 0;
+float V_refe = 5.0;
+float sensibilidade = 0.020;
+float corrente = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(sensor, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  leitura_amp = analogRead(sensor);
+  V_lida = ((leitura_amp*V_refe)/(1023.0));
+  corrente = (V_lida - (V_refe/2.0))/sensibilidade;
+  Serial.print("Corrente (Ampere): ");
+  Serial.print(corrente);
+  delay(500);
 }
